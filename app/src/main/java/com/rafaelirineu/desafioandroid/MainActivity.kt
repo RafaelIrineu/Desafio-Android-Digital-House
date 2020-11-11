da536f1beda2578b0dbbba875e04963042c68133
RafaelIrineu <rafaelirineu01@gmail.com>
2020-11-11 14:27:49 -0300

    Layout tela de registro e interacao entre telas

--- app/src/main/java/com/rafaelirineu/desafioandroid/MainActivity.kt
+++ app/src/main/java/com/rafaelirineu/desafioandroid/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.rafaelirineu.desafioandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGoToRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
