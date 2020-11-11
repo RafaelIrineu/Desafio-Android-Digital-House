5a8cc5c7aa57b4e4449227591f858e8befe93a8a
RafaelIrineu <rafaelirineu01@gmail.com>
2020-11-11 16:50:33 -0300

    Validacao de campos tela de Login

--- app/src/main/java/com/rafaelirineu/desafioandroid/MainActivity.kt
+++ app/src/main/java/com/rafaelirineu/desafioandroid/MainActivity.kt
@@ -7,17 +7,42 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnLogin.setOnClickListener {
+            if (validaCamposLogin()) {
+
+            }
+        }
+
         btnGoToRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
-
-
         }
+
+    }
+    
+    fun validaCamposLogin(): Boolean {
+
+        val emailLogin = edtEmailLogin.text.toString().trim()
+        val passwordLogin = edtPasswordLogin.text.toString().trim()
+
+        var resultado = true
+
+        if (emailLogin.isEmpty()) {
+            edtEmailLogin.error = "Campo vazio"
+            resultado = false
+        }
+        if (passwordLogin.isEmpty()) {
+            edtPasswordLogin.error = "Campo vazio"
+            resultado = false
+        }
+        return resultado
+
     }
 }
